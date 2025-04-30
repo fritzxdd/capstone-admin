9bc3ee54035a9227652eac1fa95116f36786e4f4
fritz <fritz.marlowe@gmail.com>
2025-04-30 18:00:23 +0800

    update multiple secretaries part 8.2

--- client/src/components/Secretary/index.js
+++ client/src/components/Secretary/index.js
@@ -1,24 +1,15 @@
-// Export all secretary components for easy imports
+// Import components
 import Secretary from './Secretary';
 import SecretarySelector from './SecretarySelector';
-import ManageSecretary from '../../pages/Secretary/ManageSecretary';
-import AddSecretary from '../../pages/Secretary/AddSecretary';
-import EditSecretary from '../../pages/Secretary/EditSecretary';
 
-// Export individual components
+// Export components
 export {
-    Secretary,
-    SecretarySelector,
-    ManageSecretary,
-    AddSecretary,
-    EditSecretary
+  Secretary,
+  SecretarySelector
 };
 
 // Default export
 export default {
-    Secretary,
-    SecretarySelector,
-    ManageSecretary,
-    AddSecretary,
-    EditSecretary
+  Secretary,
+  SecretarySelector
 };
